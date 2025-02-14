51b44fb7cdc4f911cfd0147994a52ca9a794ac5b
Raghav-0007 <rmyadav514@gmail.com>
2025-02-14 12:57:47 +0530

    [Radhamohan yadav] . add completed filehandling program

--- Day-03-java-streams/src/main/java/filehandling/Main.java
+++ Day-03-java-streams/src/main/java/filehandling/Main.java
@@ -1,7 +1,29 @@
 package filehandling;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        String file1 = "src/main/java/filehandling/file.txt";
+        String file2 = "src/main/java/filehandling/file2.txt";
+        try {
+            FileInputStream fis = new FileInputStream(file1);
+            InputStreamReader reader=new InputStreamReader(fis);
+            BufferedReader br=new BufferedReader(reader);
+
+            FileOutputStream fos=new FileOutputStream(file2);
+            OutputStreamWriter writer=new OutputStreamWriter(fos);
+            BufferedWriter bw=new BufferedWriter(writer);
+
+            String line;
+            while ((line=br.readLine())!=null){
+                bw.write(line);
+                bw.flush();
+                bw.newLine();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
